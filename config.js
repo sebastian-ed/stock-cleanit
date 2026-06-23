6a5c6dbfd6c1d2c487107e9e3cfcb0eac70d0784
sebastian-ed <seba020694@gmail.com>
2026-06-23 11:40:32 -0300

    Config supabase

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   APP_NAME: "Stock Clean It",
-  SUPABASE_URL: "https://TU-PROYECTO.supabase.co",
-  SUPABASE_ANON_KEY: "TU-ANON-KEY",
+  SUPABASE_URL: "https://rnplvjgfrspflgwaafxz.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_4EPKRWb9ilACu1GJw6Filw_cke4r-CY",
   STOCK_STALE_DAYS: 7,
   MATERIAL_IMAGE_BUCKET: "material-images"
 };
